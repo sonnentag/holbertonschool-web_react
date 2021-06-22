79bfbbd7b38e862d3fbfaaddb5bec69ab139dd30
Tony Sims <zocle@zocle.net>
2021-06-22 22:48:33 +0000

    added exports and revised string quotes

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -1,29 +1,29 @@
-interface DirectorInterface {
+export interface DirectorInterface {
 	workFromHome(): string;
 	getCoffeeBreak(): string;
 	workDirectorTasks(): string;
 }
 
-interface TeacherInterface {
+export interface TeacherInterface {
   workFromHome(): string;
   getCoffeeBreak(): string;
   workDirectorTasks(): string;
 }
 
 export class Director implements DirectorInterface {
-	workFromHome() { return "Working from home"; }
-	getCoffeeBreak() { return "Getting a coffee break"; }
-	workDirectorTasks() { return "Getting to director tasks"; }
+	workFromHome() { return 'Working from home'; }
+	getCoffeeBreak() { return 'Getting a coffee break'; }
+	workDirectorTasks() { return 'Getting to director tasks'; }
 }
 
 export class Teacher implements TeacherInterface {
-	workFromHome() { return "Cannot work from home"; }
-	getCoffeeBreak() { return "Cannot have a break"; }
-	workTeacherTasks() { return "Getting to work"; }
+	workFromHome() { return 'Cannot work from home'; }
+	getCoffeeBreak() { return 'Cannot have a break'; }
+	workTeacherTasks() { return 'Getting to work'; }
 }
 
 export function createEmployee(salary: number | string): Director | Teacher {
-	if (typeof salary === "number" && salary < 500) return new Teacher();
+	if (typeof salary === 'number' && salary < 500) return new Teacher();
 	return new Director();
 }
 
