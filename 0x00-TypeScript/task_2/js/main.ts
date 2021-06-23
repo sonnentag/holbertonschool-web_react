3f190c50ad01e2c2f93f3a9585f89521a8e2aa83
Tony Sims <zocle@zocle.net>
2021-06-23 00:10:43 +0000

    revised quotes

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -38,6 +38,6 @@ export function executeWork(employee: DirectorInterface | TeacherInterface): str
 type Subjects = "Math" | "History";
 
 export function teachClass(todayClass:Subjects): string {
-  if (todayClass === "Math") return `Teaching Math`;
-  else if (todayClass === "History") return `Teaching History`;
+  if (todayClass === 'Math') return "Teaching Math";
+  else if (todayClass === 'History') return "Teaching History";
 }
