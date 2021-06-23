4fbe743be78efcea1efb678f396d2b8648bc2bb6
Tony Sims <zocle@zocle.net>
2021-06-23 02:00:51 +0000

    subjects first

--- 0x00-TypeScript/task_4/js/main.ts
+++ 0x00-TypeScript/task_4/js/main.ts
@@ -1,7 +1,7 @@
+/// <reference path="./subjects/Subject.ts" />
 /// <reference path="./subjects/Cpp.ts" />
 /// <reference path="./subjects/Java.ts" />
 /// <reference path="./subjects/React.ts" />
-/// <reference path="./subjects/Subject.ts" />
 /// <reference path="./subjects/Teacher.ts" />
 
 export const cpp: Subjects.Cpp = new Subjects.Cpp();
