e30b385826fed7ba765768b82a2019af33069253
Tony Sims <zocle@zocle.net>
2021-06-24 05:12:03 +0000

    task 1

---
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,15 @@
+const $ = require( "jquery" );
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id=\'count\'></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+button.addEventListener("click", _.debounce(updateCounter));
+
+
+function updateCounter() {
+  const clicks = $('#count').html().split(" ", 1) + 1 || 1;
+  $('#count').html(`${clicks} clicks on the button`);
+}
