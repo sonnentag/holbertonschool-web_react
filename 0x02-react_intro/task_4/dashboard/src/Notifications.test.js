16ce16650cbe83311cbeac175b52e69891a77236
Tony Sims <zocle@zocle.net>
2021-07-01 14:23:47 +0000

    pushing build

---
+++ 0x02-react_intro/task_4/dashboard/src/Notifications.test.js
@@ -0,0 +1,16 @@
+import { shallow } from 'enzyme'
+import Notifications from './Notifications'
+
+const wrapper = shallow(<Notifications />)
+
+it('renders without crashing', () => {
+	expect(wrapper.exists())
+});
+
+it('renders three list items', () => {
+  expect(wrapper.find('li').children().length).toEqual(3)
+});
+
+it('renders the correct text', () => {
+  expect( wrapper.containsMatchingElement(<p>Here is the list of notifications</p>)).toBeTruthy()
+});
---
+++ 0x02-react_intro/task_4/dashboard/src/utils.test.js
@@ -0,0 +1,16 @@
+import { getFullYear, getFooterCopy, getLatestNotification } from './utils'
+
+it('getFullYear', () => {
+  expect(getFullYear()).toEqual('2021');
+});
+
+it('getFooterCopy with true or false', () => {
+  expect(getFooterCopy(true)).toEqual('Holberton School');
+  expect(getFooterCopy(false)).toEqual('Holberton School main dashboard');
+});
+
+it('getLatestNotification', () => {
+  expect(getLatestNotification()).toEqual(
+    '<strong>Urgent requirement</strong> - complete by EOD'
+  );
+});
