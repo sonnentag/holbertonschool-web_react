e5317fa3870667c36c78059e692f3509a17ea6d8
Tony Sims <zocle@zocle.net>
2021-07-06 10:47:56 +0000

    added describe

--- 0x03-react_props/task_1/dashboard/src/Notifications/Notifications.test.js
+++ 0x03-react_props/task_1/dashboard/src/Notifications/Notifications.test.js
@@ -2,16 +2,20 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import Notifications from './Notifications'
 
-const wrapper = shallow(<Notifications />)
 
-it('renders without crashing', () => {
-	expect(wrapper.exists())
-});
+describe('<Notifications />', () => {
+  const wrapper = shallow(<Notifications />)
 
-it('renders three list items', () => {
-  expect(wrapper.find('li').length).toEqual(3)
-});
+  it('renders without crashing', () => {
+  	expect(wrapper.exists())
+  });
 
-it('renders the correct text', () => {
-  expect( wrapper.containsMatchingElement(<p>Here is the list of notifications</p>)).toBeTruthy()
+  it('renders three list items', () => {
+    expect(wrapper.find('li').length).toEqual(3)
+  });
+
+  it('renders the correct text', () => {
+    expect( wrapper.containsMatchingElement(<p>Here is the list of notifications</p>)).toBeTruthy()
+  });
+
 });
