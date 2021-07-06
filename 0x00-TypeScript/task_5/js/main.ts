6c99b405e83d511b1b0f5c154ab71daa4c0ff1de
Tony Sims <zocle@zocle.net>
2021-07-06 01:40:46 +0000

    task 5 type error

--- 0x00-TypeScript/task_5/js/main.ts
+++ 0x00-TypeScript/task_5/js/main.ts
@@ -1,19 +1,17 @@
 export interface MajorCredits {
-  credit: number;
+  credits: number;
   _brand: "major";
 }
 
 export interface MinorCredits {
-  credit: number;
+  credits: number;
   _brand: "minor";
 }
 
 export function sumMajorCredits( subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
-  const sum = subject1.credit + subject2.credit;
-  return sum;
+  return { credits: subject1.credits + subject2.credits } as MajorCredits;
 }
 
 export function sumMinorCredits( subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
-  const sum = subject1.credit + subject2.credit;
-  return sum;
+	return { credits: subject1.credits + subject2.credits } as MinorCredits;
 }
