1b4ed0c2a87a19a41e91ca5f9e35ea14afe966f5
Tony Sims <zocle@zocle.net>
2021-07-15 09:39:17 +0000

    sans tests

---
+++ 0x04-React_component/task_1/dashboard/src/App/App.js
@@ -0,0 +1,70 @@
+import React from "react";
+import "../App/App.css";
+import Notifications from "../Notifications/Notifications";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import CourseList from "../CourseList/CourseList";
+import PropTypes from 'prop-types';
+import { getLatestNotification } from "../utils/utils";
+
+const listCourses = [
+  {id: 1, name: 'ES6', credit: 60},
+  {id: 2, name: 'Webpack', credit: 20},
+  {id: 3, name: 'React', credit: 40},
+];
+const listNotifications = [
+  {id: 1, type: 'default', value: 'New course available'},
+  {id: 2, type: 'urgent', value: 'New resume available'},
+  {id: 3, type: 'urgent', html: { __html: getLatestNotification()} },
+];
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+		this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  static propTypes = {
+	  logOut: PropTypes.func,
+    isLoggedIn: PropTypes.bool
+  };
+
+  static defaultProps = {
+	  logOut: () => {},
+    isLoggedIn: false
+  };
+
+  handleKeyDown(event) {
+    if (event.key === "h" && event.ctrlKey) {
+      alert("Logging you out");
+      this.props.logOut();
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  render () {
+    return (
+      <>
+        <Notifications listNotifications={listNotifications} />
+        <div className="App">
+          <Header />
+          <hr />
+            {this.props.isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+          <hr />
+          <Footer />
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
