522fd515e2fc0f6c1a34d8b15c304a2fee594fc5
Tony Sims <zocle@zocle.net>
2021-07-23 02:48:47 +0000

    app tests

--- 0x06-react_state/task_0/dashboard/src/App/App.test.js
+++ 0x06-react_state/task_0/dashboard/src/App/App.test.js
@@ -7,6 +7,14 @@ import Login from '../Login/Login';
 import Notifications from '../Notifications/Notifications';
 
 describe('<App />', () => {
+  beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+
+  afterEach(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
+
   it('App renders without crashing', () => {
     shallow(<App />)
   });
@@ -26,10 +34,29 @@ describe('<App />', () => {
   it('contains a <Notifications /> component', () => {
     shallow(<Notifications />)
   });
-/**
- * Define the propTypes and the defaultProps for the new props
- * When clicking on Your notifications, call handleDisplayDrawer
- * When clicking on the close button, call handleHideDrawer*
- */
-	
+
+  it("Has default state for displayDrawer false", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.state().displayDrawer).toEqual(false);
+  });
+
+  it("displayDrawer changes to true when calling handleDisplayDrawer", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.state().displayDrawer).toEqual(false);
+
+    const instance = wrapper.instance();
+    instance.handleDisplayDrawer();
+    expect(wrapper.state().displayDrawer).toEqual(true);
+  });
+
+  it("displayDrawer changes to false when calling handleHideDrawer", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.state().displayDrawer).toEqual(false);
+
+    wrapper.instance().handleDisplayDrawer();
+    expect(wrapper.state().displayDrawer).toEqual(true);
+
+    wrapper.instance().handleHideDrawer();
+    expect(wrapper.state().displayDrawer).toEqual(false);
+  });
 });
