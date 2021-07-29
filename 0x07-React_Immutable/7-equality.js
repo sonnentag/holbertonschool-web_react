2cbda9273ef79c42dfff005360a4663b24979f21
Tony Sims <zocle@zocle.net>
2021-07-29 00:01:37 +0000

    export at end, durp

--- 0x07-React_Immutable/7-equality.js
+++ 0x07-React_Immutable/7-equality.js
@@ -1,3 +1,5 @@
 import { is } from 'immutable';
 
-export default const areMapsEqual = (map1, map2) => is(map1, map2);
+const areMapsEqual = (map1, map2) => is(map1, map2);
+
+export default areMapsEqual;
